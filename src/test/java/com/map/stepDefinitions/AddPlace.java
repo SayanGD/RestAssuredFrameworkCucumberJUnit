0a85ea86afac988f0341ee8f2742a8d0e716f886
Sayan Ghosh Dastidar <sayangd.41@gmail.com>
2025-01-25 18:43:21 +0530

    updating step definitions code

--- src/test/java/com/map/stepDefinitions/AddPlace.java
+++ src/test/java/com/map/stepDefinitions/AddPlace.java
@@ -1,11 +1,11 @@
 package com.map.stepDefinitions;
 
 import java.io.IOException;
+import org.junit.Assert;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -19,6 +19,7 @@ public class AddPlace extends SpecificationBuilder
 
 	RequestSpecification requestSpecification;
 	Response response;
+	String responseBody;
 
 	@Given("I have the AddPlace request body")
 	public void i_have_the_add_place_request_body() throws IOException
@@ -38,10 +39,18 @@ public class AddPlace extends SpecificationBuilder
 	}
 
 	@Then("I should get a successful response with {int} status code")
-	public void i_should_get_a_successful_response_with_status_code(int responseCode)
+	public void i_should_get_a_successful_response_with_status_code(int expectedResponseCode)
 	{
-		String responseBody = response
-				.then().log().all().assertThat().statusCode(200).body("status", equalTo("OK")).body("scope", equalTo("APP")).extract().response().asString();
+		response=response.then().log().all().extract().response();
+		int actualResponseCode=response.getStatusCode();
+		Assert.assertEquals(expectedResponseCode, actualResponseCode);
+
+		responseBody = response.asString();
+	}
+
+	@Then("I should get {string} as {string}")
+	public void i_should_get_as(String attribute, String value)
+	{
 
 		JsonPath js=new JsonPath(responseBody);
 		String placeID=js.getString("place_id");
@@ -54,11 +63,5 @@ public class AddPlace extends SpecificationBuilder
 		given().spec(requestSpecification).log().all().body(deletePlaceRequestBody)
 		.when().delete("/maps/api/place/delete/json")
 		.then().log().all().assertThat().statusCode(200);
-	}
-
-	@Then("I should get {string} as {string}")
-	public void i_should_get_as(String string, String string2)
-	{
-	    
 	}
 }
