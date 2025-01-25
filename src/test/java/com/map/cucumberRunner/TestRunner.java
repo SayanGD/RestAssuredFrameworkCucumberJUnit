a8cef718ae08904bcd599d1beaacce51503f860c
Sayan Ghosh Dastidar <sayangd.41@gmail.com>
2025-01-25 20:34:06 +0530

    Added Cucumber test runner class

---
+++ src/test/java/com/map/cucumberRunner/TestRunner.java
@@ -0,0 +1,18 @@
+package com.map.cucumberRunner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="src/test/java/features",
+		glue={"com.map.stepDefinitions"},
+		tags="@Regression"
+		)
+
+
+public class TestRunner
+{
+
+}
